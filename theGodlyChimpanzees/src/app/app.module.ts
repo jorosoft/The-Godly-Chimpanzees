202a981e265b3f3127f8d586207130ccca637268
Nadezhda Hristova <epohster@gmail.com>
2017-08-25 22:27:16 +0300

    Add material design dependencies, simple buttons

--- theGodlyChimpanzees/src/app/app.module.ts
+++ theGodlyChimpanzees/src/app/app.module.ts
@@ -1,19 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdCardModule, MdButtonModule, MdSelectModule, MdSidenavModule, MdToolbarModule, MdInputModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
-import { NavComponent } from './shared/layout/nav/nav.component';
-import { FooterComponent } from './shared/layout/footer/footer.component';
+import { NavComponent } from './shared/nav/nav.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdCardModule,
+    MdButtonModule,
+    MdSelectModule,
+    MdSidenavModule,
+    MdToolbarModule,
+    MdInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
