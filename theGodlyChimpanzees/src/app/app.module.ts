c2fef4b955c7f3c628ede56b2deeaea2d0f68d0b
TsvetelinaM <tsvetka_ta@abv.bg>
2017-08-25 16:39:27 +0300

    adding home rout

--- theGodlyChimpanzees/src/app/app.module.ts
+++ theGodlyChimpanzees/src/app/app.module.ts
@@ -3,15 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    NotFoundComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
