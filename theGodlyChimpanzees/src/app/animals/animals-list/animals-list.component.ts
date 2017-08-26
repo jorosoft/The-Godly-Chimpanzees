f94d6a343e3decdb1b6e7ce7ac922c5fa679d7f9
jorosoft <jorosoftbg@gmail.com>
2017-08-27 00:04:36 +0300

    Added showing all animals list with working pagination

--- theGodlyChimpanzees/src/app/animals/animals-list/animals-list.component.ts
+++ theGodlyChimpanzees/src/app/animals/animals-list/animals-list.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { ModelsService } from './../../models.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { DataSource } from '@angular/cdk';
+import { MdPaginator } from '@angular/material';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/observable/merge';
+import 'rxjs/add/operator/map';
+import { Model } from '../../model';
 
 @Component({
   selector: 'app-animals-list',
@@ -6,10 +15,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./animals-list.component.css']
 })
 export class AnimalsListComponent implements OnInit {
+  animals;
+  exampleDatabase;
+  dataSource: ExampleDataSource | null;
 
-  constructor() { }
+  @ViewChild(MdPaginator) paginator: MdPaginator;
+
+  displayedColumns = ['name'];
+
+  constructor(private modelService: ModelsService) { }
 
   ngOnInit() {
+    this.animals = this.modelService.getAll();
+    this.exampleDatabase = new ExampleDatabase(this.animals);
+    this.dataSource = new ExampleDataSource(this.exampleDatabase, this.paginator);
+  }
+}
+
+export class ExampleDatabase {
+  /** Stream that emits whenever the data has been modified. */
+  dataChange: BehaviorSubject<Model[]>;
+
+  constructor(animals: Model[]) {
+    this.dataChange = new BehaviorSubject<Model[]>(animals);
   }
 
+  get data(): Model[] { return this.dataChange.value; }
+}
+
+export class ExampleDataSource extends DataSource<any> {
+  constructor(private _exampleDatabase: ExampleDatabase, private _paginator: MdPaginator) {
+    super();
+  }
+
+  /** Connect function called by the table to retrieve one stream containing the data to render. */
+  connect(): Observable<Model[]> {
+    const displayDataChanges = [
+      this._exampleDatabase.dataChange,
+      this._paginator.page,
+    ];
+
+    return Observable.merge(...displayDataChanges).map(() => {
+      const data = this._exampleDatabase.data.slice();
+
+      // Grab the page's slice of data.
+      const startIndex = this._paginator.pageIndex * this._paginator.pageSize;
+      return data.splice(startIndex, this._paginator.pageSize);
+    });
+  }
+
+  disconnect() {}
 }
--- theGodlyChimpanzees/src/app/animals/animals.module.ts
+++ theGodlyChimpanzees/src/app/animals/animals.module.ts
@@ -3,14 +3,20 @@ import { CommonModule } from '@angular/common';
 import { AnimalsRoutingModule } from './animals-routing.module';
 import { AnimalsListComponent } from './animals-list/animals-list.component';
 import { AnimalDetailsComponent } from './animal-details/animal-details.component';
+import { MdPaginatorModule, MdTableModule, MdGridListModule } from '@angular/material';
+import { CdkTableModule } from '@angular/cdk';
 
 @NgModule({
   imports: [
     CommonModule,
-    AnimalsRoutingModule
+    AnimalsRoutingModule,
+    CdkTableModule,
+    MdTableModule,
+    MdPaginatorModule,
+    MdGridListModule
   ],
   declarations: [
-    AnimalsListComponent,
+    AnimalsListComponent,
     AnimalDetailsComponent
 ]
 })
