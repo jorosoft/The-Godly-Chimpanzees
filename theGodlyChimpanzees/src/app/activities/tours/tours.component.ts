98953bf15720b1be9d85fe5633eff656e3faee03
TsvetelinaM <tsvetka_ta@abv.bg>
2017-09-04 14:00:33 +0300

    Adding tours values

--- theGodlyChimpanzees/src/app/activities/tours/tours.component.ts
+++ theGodlyChimpanzees/src/app/activities/tours/tours.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivitiesService } from './../../services/activities.service';
+import { Router } from '@angular/router';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/find';
 @Component({
   selector: 'app-tours',
   templateUrl: './tours.component.html',
   styleUrls: ['./tours.component.scss']
 })
 export class ToursComponent implements OnInit {
-
-  constructor() { }
+  public tours = [];
+  constructor(public activitiService: ActivitiesService, public router: Router) { }
 
   ngOnInit() {
+    this.activitiService.getTours().subscribe(value => this.tours.push(value));
   }
 
 }
