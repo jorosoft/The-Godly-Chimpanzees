c9ba9516b7f7a52104594de9470a67e4e4bd45ef
jorosoft <jorosoftbg@gmail.com>
2017-09-10 11:59:58 +0300

    Added tests for creation of services, directives and pipes

--- theGodlyChimpanzees/src/app/services/data-base.service.spec.ts
+++ theGodlyChimpanzees/src/app/services/data-base.service.spec.ts
@@ -1,15 +1,39 @@
-// import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 
-// import { DataBaseService } from './data-base.service';
+import { DataBaseService } from './data-base.service';
+import { FirebaseApp } from 'angularfire2';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
 
-// describe('DataBaseService', () => {
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       providers: [DataBaseService]
-//     });
-//   });
+describe('DataBaseService', () => {
 
-//   it('should be created', inject([DataBaseService], (service: DataBaseService) => {
-//     expect(service).toBeTruthy();
-//   }));
-// });
+    const AngularFireMocks = {
+        auth: jasmine.createSpy('auth')
+    };
+
+    const AngularFireDBMocks = {
+        database: jasmine.createSpy('database'),
+        list: jasmine.createSpy('list')
+      };
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+          DataBaseService,
+          FirebaseApp,
+          {
+            provide: AngularFireDatabase,
+            useValue: AngularFireDBMocks
+          },
+          {
+            provide: AngularFireAuth,
+            useValue: AngularFireMocks
+          },
+        ]
+    });
+  });
+
+  it('should be created', inject([DataBaseService], (service: DataBaseService) => {
+    expect(service).toBeTruthy();
+  }));
+});
