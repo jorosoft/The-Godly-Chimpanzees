dffb599dbc869d246e7ce8110417d05f85bf060c
TsvetelinaM <tsvetka_ta@abv.bg>
2017-09-10 22:19:23 +0300

    creating unit test for the activities resolvers

--- theGodlyChimpanzees/src/app/activities/ticket-resolve.spec.ts
+++ theGodlyChimpanzees/src/app/activities/ticket-resolve.spec.ts
@@ -1,15 +1,42 @@
+import { AngularFireDatabase } from 'angularfire2/database';
+import { ActivitiesService } from './../core/activities.service';
 import { TestBed, inject } from '@angular/core/testing';
+import { FirebaseApp } from 'angularfire2';
+import { TicketResolve } from './ticket-resolve';
+import { DataBaseService } from './../core/data-base.service';
+import { AngularFireAuth } from 'angularfire2/auth';
 
-import { TicketResolveService } from './ticket-resolve.service';
 
-describe('TicketResolveService', () => {
+
+describe('TicketResolve', () => {
+
+  const AngularFireMocks = {
+    auth: jasmine.createSpy('auth')
+};
+
+const AngularFireDBMocks = {
+    database: jasmine.createSpy('database'),
+    list: jasmine.createSpy('list')
+  };
+
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [TicketResolveService]
+      providers: [TicketResolve, ActivitiesService,
+                  AngularFireDatabase, FirebaseApp,
+                  DataBaseService,
+        {
+          provide: AngularFireDatabase,
+          useValue: AngularFireDBMocks
+        },
+        {
+          provide: AngularFireAuth,
+          useValue: AngularFireMocks
+        }]
     });
   });
 
-  it('should be created', inject([TicketResolveService], (service: TicketResolveService) => {
+  it('should be created', inject([TicketResolve], (service: TicketResolve) => {
     expect(service).toBeTruthy();
   }));
 });
--- theGodlyChimpanzees/src/app/activities/tours-resolve.spec.ts
+++ theGodlyChimpanzees/src/app/activities/tours-resolve.spec.ts
@@ -1,15 +1,17 @@
+import { AngularFireDatabase } from 'angularfire2/database';
+import { ActivitiesService } from './../core/activities.service';
 import { TestBed, inject } from '@angular/core/testing';
+import { FirebaseApp } from 'angularfire2';
+import { ToursResolve } from './tours-resolve';
 
-import { ToursResolveService } from './tours-resolve.service';
-
-describe('ToursResolveService', () => {
+describe('ToursResolve', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ToursResolveService]
+      providers: [ToursResolve, ActivitiesService, AngularFireDatabase, FirebaseApp]
     });
   });
 
-  it('should be created', inject([ToursResolveService], (service: ToursResolveService) => {
+  it('should be created', inject([ToursResolve], (service: ToursResolve) => {
     expect(service).toBeTruthy();
   }));
 });
